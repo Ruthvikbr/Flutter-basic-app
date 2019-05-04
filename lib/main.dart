5e316b2165facc6fed5e5eb349261ed7dc05d262
Ruthvikbr <ruthvikbr24@gmail.com>
2019-05-04 11:47:11 +0530

    Font variations

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   title: "jynx",
   home: Scaffold(
-    drawer: Drawer(),
+    drawer: Drawer(),//Drawer components should be added here
     appBar: AppBar(
       title: Text("Welcome to Flutter"),
+      //Whatever text we want to add should be added here
     ),
-    body:Center(child: Text("data",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),)),
+    body:Center(child: Text("data",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),)),
+
   ),//Scaffold
 ));
 
