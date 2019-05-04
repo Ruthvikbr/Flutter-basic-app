f53ea94f19cb0053803d25a5156ae2eb8215a4a1
Ruthvikbr <ruthvikbr24@gmail.com>
2019-05-04 12:14:04 +0530

    ListView added

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,30 @@ class HomeWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(),//Drawer components should be added here
+      drawer: Drawer(),
+      //Drawer components should be added here
+
       appBar: AppBar(
         title: Text("Welcome to Flutter"),
         //Whatever text we want to add should be added here
       ),
-      body:Center(child: Text("data",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic,),)),
-
-    );
+      body:ListView(
+        children: <Widget>[
+          Text("data"),
+          Text("data"),
+          Text("data"),
+          Text("data"),
+          Text("data"),
+          Text("data"),
+          Text("data"),
+          Text("data")
+        ],
+      ),//Listview
+floatingActionButton: FloatingActionButton(
+  onPressed: (){},
+  child: Icon(Icons.add),
+      ),//FAB
+    ); //Scaffold
     //Stateless widget is something that doesn't need UI changes
 
   }}
