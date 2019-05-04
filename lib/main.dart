171a697ff442aebc5ad7cb6e4af178b37dd77b59
Ruthvikbr <ruthvikbr24@gmail.com>
2019-05-04 12:02:06 +0530

    Stateless Widget Added

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   title: "jynx",
-  home: Scaffold(
-    drawer: Drawer(),//Drawer components should be added here
-    appBar: AppBar(
-      title: Text("Welcome to Flutter"),
-      //Whatever text we want to add should be added here
-    ),
-    body:Center(child: Text("data",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic),)),
+  home: HomeWidget()
 
-  ),//Scaffold
 ));
 
+class HomeWidget extends StatelessWidget{
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),//Drawer components should be added here
+      appBar: AppBar(
+        title: Text("Welcome to Flutter"),
+        //Whatever text we want to add should be added here
+      ),
+      body:Center(child: Text("data",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,fontStyle: FontStyle.italic,),)),
+
+    );
+    //Stateless widget is something that doesn't need UI changes
+
+  }}
