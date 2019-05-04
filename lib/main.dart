da8531c359ecbc5bdb035abda71b9d770cffafd5
Ruthvikbr <ruthvikbr24@gmail.com>
2019-05-04 16:44:58 +0530

    JSON parsing added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'as convert;
 
 void main() => runApp(MaterialApp(
   title: "jynx",
@@ -18,25 +20,70 @@ class HomeWidget extends StatefulWidget{
 
 }
 class _HomeWidgetState extends State<HomeWidget>{
-  bool color=true;
+
+  List apiData;
+
+  @override
+  void initState(){
+    super.initState();
+    print("Working");
+    fetchdata();
+  }
+
+  void fetchdata()async{
+    var url = "http://dummy.restapiexample.com/api/v1/employees";
+    print("before");
+
+    var response = await http.get(url);
+    print("response ${response}");
+    print("after");
+
+    if(response.statusCode==200){
+      var jsonResponse = convert.jsonDecode(response.body);
+      print(jsonResponse);
+      setState(() {
+        apiData=jsonResponse;
+        print(apiData.length);
+      });
+    }
+    else{
+      print("Request failed :  ${response.statusCode}");
+    }
+
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
 
     // TODO: implement build
     return Scaffold(
+      appBar: AppBar(
+        title: Text("API DATA FLOW"),
+      ),//appbar
       body: Container(
         alignment: Alignment.center,
-        color: color ? Colors.blue:Colors.red,
-        child: Text(color.toString(),style: TextStyle(fontSize:22 )),//text
+
+        child: apiData==null?CircularProgressIndicator:ListView.builder(
+        itemCount: apiData.length
+        ,itemBuilder: (context,index){
+    return ListTile(
+        leading: CircleAvatar(
+          child: Text("$index"),
+        ),//Circle avatar
+      title: Text("${apiData[index]["employee_name"]}"),
+      subtitle: Text("${apiData[index]["employee_salary"]}"),
+
+
+    );//listtile
+    }),//listview
 
       ),//container
       floatingActionButton: FloatingActionButton(
           onPressed: (){
-            print(color);
 
-            setState(() {
-              color=!color;
-            });
    },
         child: Icon(Icons.add),
       ),//FAB
